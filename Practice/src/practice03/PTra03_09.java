e7de0035a0feda719328a6c1367988eb555fdcec
RikaHirano <rkhrn.2222@gmail.com>
2018-04-12 14:44:00 +0900

    PTra03_09完了

--- Practice/src/practice03/PTra03_09.java
+++ Practice/src/practice03/PTra03_09.java
@@ -18,5 +18,42 @@ public class PTra03_09 {
 		 *
 		 * ※ プログラムは何行書いても良いです
 		 */
+
+		//入力型プログラミングとしてください
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+		/*０～６までの数字を入力してもらいます
+		 * ０：日曜～６：土曜を、それぞれ入力された数字から判断して出力します
+		 */
+		int num = scanner.nextInt();
+
+		switch(num) {
+			case 0:
+				System.out.println("日曜日");
+				break;
+			case 1:
+				System.out.println("月曜日");
+				break;
+			case 2:
+				System.out.println("火曜日");
+				break;
+			case 3:
+				System.out.println("水曜日");
+				break;
+			case 4:
+				System.out.println("木曜日");
+				break;
+			case 5:
+				System.out.println("金曜日");
+				break;
+			case 6:
+				System.out.println("土曜日");
+				break;
+			default:
+				System.out.println("0～6の整数を入力してください");
+
+		}
+
+
 	}
 }

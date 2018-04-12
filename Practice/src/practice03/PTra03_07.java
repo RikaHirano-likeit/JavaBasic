118ddca294984aa02dd680599063057a72c0dfc0
RikaHirano <rkhrn.2222@gmail.com>
2018-04-12 12:04:57 +0900

    PTra03_07完了

--- Practice/src/practice03/PTra03_07.java
+++ Practice/src/practice03/PTra03_07.java
@@ -31,6 +31,15 @@ public class PTra03_07 {
 		 * ※ プログラムは何行書いても良いです
 		 */
 
+		if(num>1&&num<6) {
+			System.out.println(num);
+		}
+
+		if(!(num>1&&num<6)){
+			System.out.println("選択したのは,1～5以外です");
+
+		}
+
+		}
 
 	}
-}

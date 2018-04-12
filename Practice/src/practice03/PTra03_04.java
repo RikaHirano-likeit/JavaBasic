589c70f2d240c3399c1cb8e8c076b5c3f9de00d3
RikaHirano <rkhrn.2222@gmail.com>
2018-04-12 11:15:19 +0900

    PTra03_04完了

--- Practice/src/practice03/PTra03_04.java
+++ Practice/src/practice03/PTra03_04.java
@@ -11,7 +11,7 @@ public class PTra03_04 {
 		// 入力型プログラムです。
 		// 下記の命令は、入力型プログラムで必要な記述になります。
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
-
+		
 		System.out.println("西暦を入力してください");
 
 		// コマンドプロンプトで入力した文字列が変数lineに代入されます

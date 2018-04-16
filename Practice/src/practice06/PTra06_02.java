4abe0fe08566098c5f15fe4c85b1d9d387716c92
RikaHirano <rkhrn.2222@gmail.com>
2018-04-16 18:14:02 +0900

    PTra06_02完了

--- Practice/src/practice06/PTra06_02.java
+++ Practice/src/practice06/PTra06_02.java
@@ -9,16 +9,24 @@ package practice06;
 public class PTra06_02 {
 	public static void main(String[] args) {
 
+
 		// ★ 20回繰り返しを行うfor文を記述してください
 		//-----for start
+		for(int i = 0; i<=20; i++) {
 
 			// ★ 今何回目の処理なのかを出力してください
+			System.out.print(i+"回目");
 
 
 			// ★ 奇数回のときには、「奇数」を出力してください
+			if(i%2 == 1) {
+			System.out.print(":奇数");
 
+			}
+			System.out.println();
 
-
+		}
 		//-----for end
 	}
+
 }

982915f10a968ac941d00e21693d3a93c8db16d9
RikaHirano <rkhrn.2222@gmail.com>
2018-04-17 14:48:46 +0900

    PTra07_02間違っている箇所修正

--- Practice/src/practice07/PTra07_02.java
+++ Practice/src/practice07/PTra07_02.java
@@ -14,11 +14,12 @@ public class PTra07_02 {
 		int[] array = new int[1000];
 
 		// ★ 配列arrayに [1,2,3,4,5,6,7,・・・・,998,999,1000] を代入してください
-			for(int i = 1; i<=array.length; i++)	{
+			for(int i = 0; i<array.length; i++)	{
+				array[i] = i+1;
 
 
 		// ★ 変数arrayに格納されている値を前から順番に全て表示してください
-			System.out.println(i);
+			System.out.println(array[i]);
 		}
 	}
 }

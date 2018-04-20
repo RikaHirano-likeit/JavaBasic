060b890830882be2c9866871ec8b902bbd3f956b
RikaHirano <rkhrn.2222@gmail.com>
2018-04-20 11:19:41 +0900

    PTra_01完了

--- Practice/src/practice08/PTra08_01.java
+++ Practice/src/practice08/PTra08_01.java
@@ -10,9 +10,10 @@ public class PTra08_01 {
 	public static void main(String[] args) {
 
 		// ★ 変数receiveを宣言し、メソッドnumberを実行した結果を代入してください
-
+		int receive = number();
 
 		// ★ 変数receiveを出力してください
+			System.out.println(receive);
 
 	}
 
@@ -27,5 +28,10 @@ public class PTra08_01 {
 	 */
 	public static int number() {
 		return 100;
+		
+		
+		
+		
+		
 	}
 }

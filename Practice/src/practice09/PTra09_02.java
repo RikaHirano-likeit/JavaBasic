c93c513684f5d065ed55d1cdd4c9be99dd739fd0
RikaHirano <rkhrn.2222@gmail.com>
2018-04-24 18:05:55 +0900

    PTra09_02完了

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -1,4 +1,7 @@
 package practice09;
+
+import practice09.util.Util;
+
 /*
  * PTra09_02.java
  *   作成	LIKEIT	2017
@@ -13,8 +16,12 @@ public class PTra09_02 {
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
 
+		String[] rondom = Util.shuffleArray(array);
 
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
+		for(int i =0 ; i < rondom.length ; i++) {
+			System.out.println(rondom[i]);
+		}
 
 	}
 }
